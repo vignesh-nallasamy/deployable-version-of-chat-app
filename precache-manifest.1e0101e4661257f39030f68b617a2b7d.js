010aa282a65558b2169210a3a295f727bb5535b7
vignesh <vigneshnallasamycse@gmail.com>
2020-06-28 22:56:45 +0530

    chat app

---
+++ precache-manifest.1e0101e4661257f39030f68b617a2b7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46de193356c4b01a18ee65ac7ac67f98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e795debec16efd7598ee",
+    "url": "/static/css/main.cc11034f.chunk.css"
+  },
+  {
+    "revision": "92d0ddfefa475b45c30a",
+    "url": "/static/js/2.042871d4.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/static/js/2.042871d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e795debec16efd7598ee",
+    "url": "/static/js/main.19deffbc.chunk.js"
+  },
+  {
+    "revision": "b79d61d614db827975ea",
+    "url": "/static/js/runtime-main.f23d09bb.js"
+  }
+]);
